5a2cceef2989ed1e6743cc527000d998af98447a
Anthony <secondworld.dev@gmail.com>
2023-10-02 12:09:09 -0700

    remove inline styles in TeamSummary

--- app/components/players/Dashboard/LastGame/index.js
+++ app/components/players/Dashboard/LastGame/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
-import SideBySideBarGraph from '../../../../components/common/SideBySideBar';
+
 import {
   View,
   TouchableOpacity,
@@ -15,10 +15,11 @@ import {
   Modal,
   ScrollView,
 } from 'react-native';
-import FastImage from 'react-native-fast-image';
-import {Layout, Colors, Fonts} from './../../../../constants';
+
+import {Layout, Colors} from './../../../../constants';
 import styles from './styles';
 import PlayerSummary from './PlayerSummary';
+import TeamSummary from './TeamSummary';
 let wide = Layout.width;
 
 function LastGameSection() {
@@ -53,100 +54,7 @@ function LastGameSection() {
             marginTop: wide * 0.04,
           }}>
           <PlayerSummary />
-          <View
-            style={{
-              width: '92%',
-              alignSelf: 'center',
-            }}>
-            <Text
-              style={{
-                color: Colors.light,
-                fontFamily: Fonts.Bold,
-                fontSize: 16,
-                lineHeight: 22,
-                fontWeight: '700',
-                marginLeft: wide * 0.015,
-                marginTop: wide * 0.06,
-              }}>
-              Team Summary
-            </Text>
-
-            <View style={{marginTop: wide * 0.04}}>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  width: '90%',
-                  alignSelf: 'center',
-                  // backgroundColor: 'red'
-                }}>
-                <FastImage
-                  source={require('../../../../assets/images/dummyTeamLogo.png')}
-                  // source={{
-                  //   uri: coachDashTeamData?.lastGameInfo?.teamSummary
-                  //     ?.gameStats[0].teamLogoUrl,
-                  // }}
-                  style={{
-                    width: wide * 0.1,
-                    height: wide * 0.1,
-                  }}
-                  // resizeMode={'contain'}
-                />
-                {/* :
-            <></>
-          } */}
-                <Text
-                  style={{
-                    color: Colors.light,
-                    fontFamily: Fonts.Regular,
-                    fontSize: 14,
-                    lineHeight: 20,
-                    fontWeight: '400',
-                    // marginLeft: wide * 0.015,
-                    // marginTop: wide * 0.06,
-                  }}>
-                  Team Stats
-                </Text>
-                {/* {coachDashTeamData?.lastGameInfo?.teamSummary?.gameStats != null ? */}
-
-                <FastImage
-                  source={require('../../../../assets/images/dummyTeamLogo1.png')}
-                  //   source={{
-                  //     uri: coachDashTeamData?.lastGameInfo?.teamSummary
-                  //       ?.gameStats[1].teamLogoUrl,
-                  //   }}
-                  style={{
-                    width: wide * 0.1,
-                    height: wide * 0.1,
-                  }}
-                />
-                {/* :
-            <></>
-          } */}
-              </View>
-            </View>
-
-            <View
-              style={{
-                marginTop: wide * 0.05,
-                marginBottom: wide * 0.06,
-              }}>
-              <SideBySideBarGraph
-                // pgsData={this.state.sideBySideBarchartData}
-                pgsData={[
-                  {name: 'PST', value: [12.7, 10.7]},
-                  {name: 'AST', value: [7.7, 8.7]},
-                  {name: 'RPG', value: [10.9, 11.9]},
-                  {name: 'BPG', value: [12.7, 10.7]},
-                  {name: 'STL', value: [7.7, 8.7]},
-                  {name: 'FG%', value: [10.9, 11.9]},
-                  {name: '2PT', value: [7.7, 8.7]},
-                  {name: '3PT', value: [10.9, 11.9]},
-                ]}
-              />
-            </View>
-          </View>
+          <TeamSummary />
         </View>
       </View>
     );
