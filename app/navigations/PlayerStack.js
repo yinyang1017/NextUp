f6bbcfa8e0966075c86f450e79659a9599584bc2
Anthony <secondworld.dev@gmail.com>
2023-10-10 11:36:22 -0700

    hot fix: rename module

--- app/navigations/PlayerStack.js
+++ app/navigations/PlayerStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {customTheme} from '../constants';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Dashboard from '../views/players/Dashboard';
+import Dashboard from '../views/players/DashBoard';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {
   faCalendar,
