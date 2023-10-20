bfc75f78fd53bc3c72a60515626e151ca88f89c3
piyushprashant93 <piyushprashant93@gmail.com>
2023-10-20 21:02:14 +0530

    Fix: Onboarding fixes with input

---
+++ app/views/players/MyChallenges/Challenges.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native-ui-lib';
+import { ViewContainer } from '../../../components/common/ViewConatiner';
+export default function Challenges() {
+    return (
+        <ViewContainer headerTilte={'My Challenges'}>
+            <Text >Challanges</Text>
+        </ViewContainer>
+    );
+}
